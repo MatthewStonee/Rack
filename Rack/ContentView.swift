8d8406c0b69ceef60d75b1385077010d157fe8bc
Matthew <mattstone01s@gmail.com>
2026-03-31 14:46:40 -0400

    Initial Commit

---
+++ Rack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rack
+//
+//  Created by Matthew Stone on 3/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rack/RackApp.swift
@@ -0,0 +1,17 @@
+//
+//  RackApp.swift
+//  Rack
+//
+//  Created by Matthew Stone on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct RackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
